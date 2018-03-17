340a8f088f9f924a3b297930ecf0b51fd9e1225f
aakash chandhoke <aakash chandhoke>
2018-03-17 18:11:40 +0530

    Initial commit

---
+++ src/java/com/Numbers/Numbers.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.Numbers;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Mithu
+ */
+@WebService(serviceName = "Numbers")
+public class Numbers {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+    @WebMethod(operationName = "prime")
+    //Range Prime Numbers
+    public String primeRange(@WebParam(name = "low")int low,@WebParam(name = "high")int high)
+    {
+        StringBuilder sb=new StringBuilder();
+        while (low <= high) 
+        {
+            if(checkPrimeNumber(low))
+                sb.append(low+"\n");
+            ++low;
+        }
+        //System.out.println(sb);
+        return sb.toString();
+    }
+    //Checking PrimeNumber
+    private boolean checkPrimeNumber(int num) 
+    {
+        boolean flag = true;
+        for(int i = 2; i <= num/2; ++i) {
+
+            if(num % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+    
+     @WebMethod(operationName = "composite")
+    //Composite Range
+    public String compositeRange(@WebParam(name = "low")int low,@WebParam(name = "high")int high)
+    {
+        StringBuilder sb=new StringBuilder();
+        while (low <= high) 
+        {
+            if(!checkPrimeNumber(low))
+                sb.append(low+"\n");
+            ++low;
+        }
+        //System.out.println(sb);
+        return sb.toString();
+    }
+    
+    @WebMethod(operationName = "perfectSquare")
+    //Perfect Square
+    public static StringBuilder perfectSquaresRange(@WebParam(name = "low")int low,@WebParam(name = "high")int high)
+    {
+        StringBuilder sb=new StringBuilder();
+        int count=0;
+        for(int x=low;x<=high;x++)
+        {
+            double d=Math.sqrt(x);
+            int d1=(int)Math.sqrt(x);
+            if(d-d1>0)
+                continue;
+            else
+            {
+                sb.append(x);
+                sb.append("\n");
+            }
+        }
+        return sb;
+    }
+}
